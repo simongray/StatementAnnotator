65710d364107d483c34b63af69c80139e2f62fca
Simon Gray <simongray@gmail.com>
2016-04-08 16:58:49 +0800

    exploring the Tree and the CoreLabel classes

--- src/main/java/demo/TestStuff.java
+++ src/main/java/demo/TestStuff.java
@@ -1,6 +1,7 @@
 package demo;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
@@ -10,6 +11,15 @@ import edu.stanford.nlp.util.CoreMap;
 
 import java.util.List;
 import java.util.Properties;
+
+/*
+    http://nlp.stanford.edu/nlp/javadoc/javanlp/
+
+    http://stackoverflow.com/questions/19884515/adding-a-new-annotator-in-stanford-corenlp
+
+
+ */
+
 
 public class TestStuff {
     public static void main(String[] args) {
@@ -34,10 +44,50 @@ public class TestStuff {
 
     }
 
+    // Tree.getSpan() = IntPair (beginning word index, end word index) of phrase in sentence
+    // Tree.label() = nodeString() = alternates between parser symbol or word, label() returns a Label object
+    // Tree.value() = same as label() but with added sentiment analysis score
+    // Tree.yield() = list of words and punctuation represented by this node in the tree
+    // Tree.dependencies() --> nullpointerexception
+    // Tree.pennString() = the indented, lisp-style parsing output
+    // Tree.constituents() = not quite sure, it's an array of (x, y) pairs, perhaps to do with dependency grammar?
+    // Tree.labeledYield() = array of "word/symbol" strings
+    // Tree.isPhrasal() = true for list of words, but not for single words or empty nodes
+    // Tree.score() = NaN
+    // Tree.labels() = array of either word for single-word nodes or a list of words and symbols in no particular order
+    // Tree.taggedLabeledYield() = list of CoreLabels that go "symbol-index" when toString()'d
+        // CoreLabel.word() = the word
+        // CoreLabel.value() = the parser symbol thingy
+        // CoreLabel.lemma() = null, even though I did include that Annotator
+        // CoreLabel.tag() = the parser symbol thingy again for some reason
+        // CoreLabel.ner() = null, even though I did include that Annotator
+    // Tree.isLeaf() = is a leaf, i.e. no children, in so in this case all of them are words (not symbols)
+    // Tree.isPreTerminal() = right before a leaf, i.e. the symbol right before a leaf (containing a word)
+    // Tree.isUnaryRewrite() = if node only has a single child, seems to be the same as isPreTerminal()
+    // Tree.isEmpty() = false in every case, seems useless
+    // Tree.isPrePreterminal() = node before a preterminal node, not sure how it can be used
+
+
+
+
+
     // recursively display scores in tree
     public static void parse(Tree tree, int n) {
-        // RNNCoreAnnotations.getPredictedClass() returns the sentiment analysis score from 0 to 4 (with -1 for n/a)
-        System.out.println(new String(new char[n]).replace("\0", " ") + tree.value() + " " + RNNCoreAnnotations.getPredictedClass(tree));
+        // RNNCoreAnnotations.getPredictedClass(tree) returns the sentiment analysis score from 0 to 4 (with -1 for n/a)
+        System.out.println(new String(new char[n]).replace("\0", " ")
+                + tree.value()
+        );
+
+        for (CoreLabel label : tree.taggedLabeledYield()) {
+            System.out.println("-----");
+            System.out.println("toString= " + label.toString());
+            System.out.println("word= " + label.word());
+            System.out.println("value= " + label.value());
+            System.out.println("lemma= " + label.lemma());
+            System.out.println("tag= " + label.tag());
+            System.out.println("ner= " + label.ner());
+            System.out.println();
+        }
 
         // scores (not confirmed, but 99% sure)
             // very negative: 0
