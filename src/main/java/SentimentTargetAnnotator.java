9bf0780205e58906d6bb84bce3de4bd69b11d1a4
Simon Gray <simongray@gmail.com>
2016-04-11 15:50:26 +0800

    added steps to implement in annotator

--- src/main/java/SentimentTargetAnnotator.java
+++ src/main/java/SentimentTargetAnnotator.java
@@ -9,6 +9,16 @@ public class SentimentTargetAnnotator implements Annotator {
 
     @Override
     public void annotate(Annotation annotation) {
+        // locate candidate entities
+
+        // determine interestingness of entities and purge uninteresting ones
+
+        // determine entities position in sentiment tree
+
+        // find conflicts between entities by moving up tree to see where each entity meets
+
+        // use heuristic(s) to select dominant entity
+        // will need to look at different trees to determine best splitting heuristic
     }
 
     @Override
