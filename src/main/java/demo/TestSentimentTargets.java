d94385c5eb4d7586e39d122cdc3dfb96e9e0c6fe
Simon Gray <simongray@gmail.com>
2016-05-02 13:52:43 +0800

    more example sentences

--- src/main/java/demo/TestSentimentTargets.java
+++ src/main/java/demo/TestSentimentTargets.java
@@ -28,7 +28,10 @@ public class TestSentimentTargets {
 
         // annotate a piece of text
         DemoTimer.start("creating annotation");
-//
+
+//        String example = "The European Parliament with its proportional representation is a much more democratic institution than the UK parliament...\n" +
+//                "I'm sure you meant the EU as a whole and not the European Parliament specifically, just thought it was funny that a Brit would complain about this when the UK voting system is one of the absolute worst in the world at representing the will of the people + your other house consists of a bunch of noble people.";
+//////
 //        String example = "Chai Jing started making the documentary when her as yet unborn daughter developed a tumour in the womb, which had to be removed very soon after her birth. Chai blames air pollution for the tumour. The film, which combines footage of a lecture with interviews and factory visits, has been compared with Al Gore's An Inconvenient Truth in both its style and likely impact. The film openly criticises state-owned energy companies, steel producers and coal factories, as well as showing the inability of the Ministry of Environmental Protection to act against the big polluters.\n" +
 //                "\n" +
 //                "Despite demonstrating the failure of China's regulations on pollution, the Chinese government at first did not censor the film. Instead, the People's Daily reposted the film alongside an interview with Chai, while Chen Jining, the recently appointed minister for environmental protection, praised the film, comparing its significance with Silent Spring, the 1962 book by US environmentalist Rachel Carson. However, within a week, the Communist Party’s publicity department confidentially ordered the film to be removed. An employee of China Business News was suspended for leaking the order.";
@@ -41,27 +44,27 @@ public class TestSentimentTargets {
 //                "Don't worry too much. Sometimes there's no smog for a whole week, sometime's it lasts for a whole week and you'll just stay mostly indoors and use masks when outside. You'll get used to it.";
 
 
-//        String example = "Clinton is a cold person and a terrible human being, unlike Bernie Sanders who seems quite competent and like a true statesman. In fact, Sanders deserves to win, Hillary Clinton is just there to earn more money. She doesn't want to help the people, she is just doing it for herself.";
+        String example = "Clinton is a cold person and a terrible human being, unlike Bernie Sanders who seems quite competent and like a true statesman. I really like Sanders, especially his ideas. In fact, Sanders deserves to win, Hillary Clinton is just there to earn more money. She doesn't want to help the people, she is just doing it for herself.";
 
-//        Annotation annotation = new Annotation(example);
-        Annotation annotation = new Annotation(
-                "George Bush was quite sentimental in his old days. " +
-                "George often thought highly of Clinton. " +
-                "He also really liked Hillary. Hillary Rodham, that is. " +
-                "She had a stranglehold on his mind and he couldn't stop thinking of her. " +
-                "In fact, Bush hated her, that piece of shit \"I hate Hillary Rodham Clinton so much\", he said to himself. " +
-                "He did like Bill Clinton, though. " +
-                "He was such as nice guy. "
-        );
+        Annotation annotation = new Annotation(example);
+//        Annotation annotation = new Annotation(
+//                "George Bush was quite sentimental in his old days. " +
+//                "George often thought highly of Clinton. " +
+//                "He also really liked Hillary. Hillary Rodham, that is. " +
+//                "She had a stranglehold on his mind and he couldn't stop thinking of her. " +
+//                "In fact, Bush hated her, that piece of shit \"I hate Hillary Rodham Clinton so much\", he said to himself. " +
+//                "He did like Bill Clinton, though. " +
+//                "He was such as nice guy. "
+//        );
         // NOTE TO SELF: common source of errors -> forgetting to put a space after every sentence
 
-        String singleTypeAntencedentsExample =
+//        String singleTypeAntencedentsExample =
 //                "Bill Clinton is an idiot. He is so stupid, I can't believe it and neither can she - or they, for that matter. " +
 //                "Unfortunately, Hillary Clinton loves him. She loves him like no one else does. " +
 //                "George Bush doesn't love Hillary or Bill Clinton. He just loves himself. And perhaps her too. "
-                "George and Hillary had a fling once. She is not over it yet. " +  // female antecedent
-                "Google is an organization, unlike Hillary Clinton or George Bush. They don't particularly like anyone. " +  // plural antecedent
-                "Bill Clinton and Google haven't had much interaction. He just goes over there sometimes. "  // male antecedent
+//                "George and Hillary had a fling once. She is not over it yet. " +  // female antecedent
+//                "Google is an organization, unlike Hillary Clinton or George Bush. They don't particularly like anyone. " +  // plural antecedent
+//                "Bill Clinton and Google haven't had much interaction. He just goes over there sometimes. "  // male antecedent
         ;
 //        Annotation annotation = new Annotation(singleTypeAntencedentsExample);
 
