cf39241a1066c8eedf4c2a204c11ac00ebcabf96
Simon Gray <simongray@gmail.com>
2016-05-10 14:46:53 +0200

    now using ComplexSentiment to implement same functionality as in previous commit

---
+++ src/main/java/sentiment/ComplexSentiment.java
@@ -0,0 +1,52 @@
+package sentiment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexSentiment {
+    final Logger logger = LoggerFactory.getLogger(SentimentProfile.class);
+    List<SentimentTarget> mentions = new ArrayList<>();
+
+    public void addMentions(List<SentimentTarget> mentions) {
+        this.mentions.addAll(mentions);
+    }
+
+    /**
+     * Get the composed sentiment of a list of mentions to the same entity.
+     * @return integer from 0 through 4
+     */
+    public int getComposedSentiment() {
+        double sum = 0.0;
+        int n = 0;
+
+        for (SentimentTarget mention : mentions) {
+            if (mention.hasSentiment()) {
+                int sentiment = mention.getSentiment();
+
+                // TODO: figure out if including (and weighting) the neutral sentiment slightly might be more precise
+                // TODO: another option is to make more complicated sentiment assessments, e.g. pos+neg=mixed
+                if (sentiment != 2) {  // only use non-neutral sentiment to calculate composed sentiment
+                    sum += mention.getSentiment();
+                    n++;
+                }
+            } else {
+                logger.error("mention has no sentiment: " + mention);
+            }
+        }
+
+        // return neutral (= 2) in case the list contained no sentiment
+        if (n == 0) {
+            return 2;
+        }
+
+        return (int) Math.round(sum / n);
+    }
+
+    @Override
+    public String toString() {
+        return getComposedSentiment() + ":" + mentions.size();
+    }
+}
--- src/main/java/sentiment/SentimentProfile.java
+++ src/main/java/sentiment/SentimentProfile.java
@@ -10,8 +10,7 @@ public class SentimentProfile {
     final Logger logger = LoggerFactory.getLogger(SentimentProfile.class);
     private final String name;
     private List<Annotation> annotations = new ArrayList<>();
-    private Map<String, List<SentimentTarget>> mentionsPerEntity = new HashMap<>();
-    private Map<String, Integer> scorePerEntity = new HashMap<>();
+    private Map<String, ComplexSentiment> entityToSentiment = new HashMap<>();
 
     public SentimentProfile(String name, List<Annotation> annotations) {
         logger.info("created sentiment profile for " + name);
@@ -25,15 +24,15 @@ public class SentimentProfile {
 
     @Override
     public String toString() {
-        return name + " " + Arrays.asList(scorePerEntity);
+        return name + " " + Arrays.asList(entityToSentiment);
     }
 
     /**
      * Get the composed scored for all entities.
      * @return map of entity to sentiment score
      */
-    public Map<String, Integer> getEntityScores() {
-        return new HashMap<>(scorePerEntity);
+    public Map<String, ComplexSentiment> getEntityScores() {
+        return new HashMap<>(entityToSentiment);
     }
 
     /**
@@ -45,60 +44,15 @@ public class SentimentProfile {
         logger.info("adding " + annotations.size() + " annotations to the profile of " + name);
         this.annotations.addAll(annotations);
 
-        // add new mentions to global mentions map
+        // add new mentions to global map of entity to sentiment
         for (Annotation annotation : annotations) {
-            Map<String, List<SentimentTarget>> localMentionsPerEntity = annotation.get(MergedSentimentTargetsAnnotation.class);
+            Map<String, List<SentimentTarget>> entityToMentions = annotation.get(MergedSentimentTargetsAnnotation.class);
 
-            for (Map.Entry<String, List<SentimentTarget>> entry : localMentionsPerEntity.entrySet()) {
-                List<SentimentTarget> mentions = mentionsPerEntity.getOrDefault(entry.getKey(), new ArrayList<>());
-                mentions.addAll(entry.getValue());
-                mentionsPerEntity.put(entry.getKey(), mentions);
+            for (Map.Entry<String, List<SentimentTarget>> entry : entityToMentions.entrySet()) {
+                ComplexSentiment sentiment = entityToSentiment.getOrDefault(entry.getKey(), new ComplexSentiment());
+                sentiment.addMentions(entry.getValue());
+                entityToSentiment.put(entry.getKey(), sentiment);
             }
         }
-
-        // (re-)calculate composed scores based on global map
-        calculateSentimentScores();
-    }
-
-    /**
-     * Go through all mentions and calculate composed sentiment scores.
-     */
-    private void calculateSentimentScores() {
-        logger.info("calculating sentiment scores for the profile of " + name);
-        for (Map.Entry<String, List<SentimentTarget>> entry : mentionsPerEntity.entrySet()) {
-            int score = getComposedSentiment(entry.getValue());
-            scorePerEntity.put(entry.getKey(), score);
-        }
-    }
-
-    /**
-     * Get the composed sentiment of a list of mentions to the same entity.
-     * @param targets
-     */
-    private int getComposedSentiment(List<SentimentTarget> targets) {
-        double sum = 0.0;
-        int n = 0;
-
-        for (SentimentTarget target : targets) {
-            if (target.hasSentiment()) {
-                int sentiment = target.getSentiment();
-
-                // TODO: figure out if including (and weighting) the neutral sentiment slightly might be more precise
-                // TODO: another option is to make more complicated sentiment assessments, e.g. pos+neg=mixed
-                if (sentiment != 2) {  // only use non-neutral sentiment to calculate composed sentiment
-                    sum += target.getSentiment();
-                    n++;
-                }
-            } else {
-                logger.error("target has no sentiment: " + target);
-            }
-        }
-
-        // return neutral (= 2) in case the list contained no sentiment
-        if (n == 0) {
-            return 2;
-        }
-
-        return (int) Math.round(sum / n);
     }
 }
