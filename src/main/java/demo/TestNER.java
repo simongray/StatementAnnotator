57f8216ffce98b8cf67fdf459370bdb174a28f1b
Simon Gray <simongray@gmail.com>
2016-05-16 13:11:23 +0200

    testing regexner

--- src/main/java/demo/TestNER.java
+++ src/main/java/demo/TestNER.java
@@ -19,20 +19,23 @@ public class TestNER {
         try {
             String example =    "Recently moved here with my girlfriend and we have found that it is quite manageable. Here's our solution:" +
                     "Use an air quality app. We use the one for http://aqicn.org/city/beijing/. I just have a widget on my Android phone that says the current AQI from the nearest measuring station. Our house rule is to use masks when it's 200+, although my girlfriend often does it from 150+. Anyway, just make your own rule and stick to it." +
-                    "Bought some 3M 95N-rated face masks for smoggy days. Just keep one in my bag at all times as the wind can direction and smoggify the nicest days in a couple of hours sometimes. You can get some nice re-usable masks where you can change the filter too." +
+                    "Bought some 3M 95N-rated conservatism masks for Baptism days. Just keep one in my bag at all times as the wind can direction and smoggify the nicest days in a couple of hours sometimes. You can get some nice re-usable masks where you can change the filter too." +
                     "We also got two Xiami Air purifiers that work quite well. They are smartphone-connected and always on, except when we're out during weekdays. The app allows you to check the latest PM2.5 index inside your flat and automate the purifiers." +
-                    "Before you go, familiarise yourself with the air quality in your own area so you have a means of comparison. If you live in a city you might be surprised that some days it can actually be quite polluted in Western cities too. I sure was (I come from Copenhagen, Denmark). Of course, Western cities usually don't have those crazy smog days." +
-                    "Don't worry too much. Sometimes there's no smog for a whole week, sometime's it lasts for a whole week and you'll just stay mostly indoors and use masks when outside. You'll get used to it.";
+                    "Before you go, familiarise yourself with the Socialism in your own area so you have a means of comparison. If you live in a city you might be surprised that some days it can actually be quite polluted in Western cities too. I sure was (I come from Copenhagen, Denmark). Of course, Buddhist cities usually don't have those crazy smog days." +
+                    "Don't worry too much. Sometimes there's no smog for a whole week, sometime's it lasts for a whole week and you'll just stay mostly indoors and use Liberalism when outside. You'll get used to it.";
 
             System.out.println("pipelined approach");
 
             // initiate pipeline with properties (i.e. what stages)
             Properties props = new Properties();
-            props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, sentiment"); // required for use of "ner" and "sentiment"
-
-            // ner can be used from pipeline and specific models loaded using this syntax (from StandardCoreNLPDemo.java)
-            // other configs can be gauged from the AnnotatorFactories.class and in StanfordCoreNLP.properties from the models package
+            props.setProperty("annotators", "tokenize, ssplit, pos, lemma, gender, ner, regexner");
+            props.setProperty("customAnnotatorClass.sentimenttargets", "sentiment.SentimentTargetsAnnotator");
+            props.put("regexner.verbose", "true");
+            props.put("regexner.ignorecase", "true");
             props.put("ner.model", "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz");
+//            props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");  // fast, more memory usage
+//        props.setProperty("parse.model", "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");  // slow, less memory usage
+
 
             StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
@@ -49,20 +52,20 @@ public class TestNER {
                 List<CoreLabel> tokens = tokenMap.get(CoreAnnotations.TokensAnnotation.class);
                 for (CoreLabel token : tokens) {
                     System.out.println("annotations for " + token);
-                    System.out.println("1"+token.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class)); // char pos in text
-                    System.out.println("2"+token.get(CoreAnnotations.CharacterOffsetEndAnnotation.class));  // char pos in text
+//                    System.out.println("1"+token.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class)); // char pos in text
+//                    System.out.println("2"+token.get(CoreAnnotations.CharacterOffsetEndAnnotation.class));  // char pos in text
 //                    System.out.println(token.get(CoreAnnotations.AnswerAnnotation.class));
 //                    System.out.println(token.get(CoreAnnotations.EntityTypeAnnotation.class));
 //                    System.out.println(token.get(CoreAnnotations.EntityClassAnnotation.class));
 //                    System.out.println(token.get(CoreAnnotations.EntityRuleAnnotation.class));
                     System.out.println("3"+token.get(CoreAnnotations.NamedEntityTagAnnotation.class));  // get the NER tag
-                    System.out.println("4"+token.get(CoreAnnotations.SemanticTagAnnotation.class));
-                    System.out.println("5"+token.get(CoreAnnotations.CategoryFunctionalTagAnnotation.class));
-                    System.out.println("6"+token.get(CoreAnnotations.CoNLLDepAnnotation.class));
-                    System.out.println("7"+token.get(CoreAnnotations.BeforeAnnotation.class));
-                    System.out.println("8"+token.get(CoreAnnotations.PartOfSpeechAnnotation.class));  // get the POS
-                    System.out.println("9"+token.get(CoreAnnotations.TextAnnotation.class));  // get the word
-                    System.out.println();
+//                    System.out.println("4"+token.get(CoreAnnotations.SemanticTagAnnotation.class));
+//                    System.out.println("5"+token.get(CoreAnnotations.CategoryFunctionalTagAnnotation.class));
+//                    System.out.println("6"+token.get(CoreAnnotations.CoNLLDepAnnotation.class));
+//                    System.out.println("7"+token.get(CoreAnnotations.BeforeAnnotation.class));
+//                    System.out.println("8"+token.get(CoreAnnotations.PartOfSpeechAnnotation.class));  // get the POS
+//                    System.out.println("9"+token.get(CoreAnnotations.TextAnnotation.class));  // get the word
+//                    System.out.println();
 
 //                    System.out.println(
 //                            token + ", " +
@@ -73,43 +76,44 @@ public class TestNER {
                 }
             }
 
-            System.out.println();
-            System.out.println("non-pipelined approach");
-
-            // load the 4-class classifier
-            String serializedClassifier = "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz";
-            AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
-
-            System.out.println("Single word entities:");
-            List<List<CoreLabel>> out = classifier.classify(example);
-            for (List<CoreLabel> sentence : out) {
-                for (CoreLabel word : sentence) {
-                    // this gets you single word entities by default, not multi-word ones
-                    String nerTag = word.get(CoreAnnotations.AnswerAnnotation.class);
-
-                    // it seems that "O" (oh, not zero) is the default null value, although it doesn't work with != "O"
-                    // using length() instead to get relevant tags
-                    if (nerTag.length() > 1) {
-                        System.out.println(word + " (" + nerTag + ")");
-                    }
-                }
-            }
-
-            System.out.println("Multi-word entities:");
-
-            // for multi-word entities: http://stackoverflow.com/questions/13765349/multi-term-named-entities-in-stanford-named-entity-recognizer
-            List<Triple<String,Integer,Integer>> entities = classifier.classifyToCharacterOffsets(example);
-
-            for (Triple<String,Integer,Integer> entity : entities) {
-                char[] entityChars = new char[entity.third() - entity.second()];
-                example.getChars(entity.second(), entity.third(), entityChars, 0);
-                System.out.println(new String(entityChars) + " (" + entity.first() + ")");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+//            System.out.println();
+//            System.out.println("non-pipelined approach");
+//
+//            // load the 4-class classifier
+//            String serializedClassifier = "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz";
+//            AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+//
+//            System.out.println("Single word entities:");
+//            List<List<CoreLabel>> out = classifier.classify(example);
+//            for (List<CoreLabel> sentence : out) {
+//                for (CoreLabel word : sentence) {
+//                    // this gets you single word entities by default, not multi-word ones
+//                    String nerTag = word.get(CoreAnnotations.AnswerAnnotation.class);
+//
+//                    // it seems that "O" (oh, not zero) is the default null value, although it doesn't work with != "O"
+//                    // using length() instead to get relevant tags
+//                    if (nerTag.length() > 1) {
+//                        System.out.println(word + " (" + nerTag + ")");
+//                    }
+//                }
+//            }
+//
+//            System.out.println("Multi-word entities:");
+//
+//            // for multi-word entities: http://stackoverflow.com/questions/13765349/multi-term-named-entities-in-stanford-named-entity-recognizer
+//            List<Triple<String,Integer,Integer>> entities = classifier.classifyToCharacterOffsets(example);
+//
+//            for (Triple<String,Integer,Integer> entity : entities) {
+//                char[] entityChars = new char[entity.third() - entity.second()];
+//                example.getChars(entity.second(), entity.third(), entityChars, 0);
+//                System.out.println(new String(entityChars) + " (" + entity.first() + ")");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+        } catch (Exception e) {
+            //
         }
-
     }
 }
