12dc096d1431d42aba052f1ee441b689796318b0
Simon Gray <simongray@gmail.com>
2016-05-18 17:20:37 +0200

    starting work on #21

---
+++ src/main/java/nlp/SubjectAnnotator.java
@@ -0,0 +1,47 @@
+package nlp;
+
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.Annotator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+/**
+ * Find the subjects in sentences and annotates them.
+ */
+public class SubjectAnnotator implements Annotator {
+    public final static String SUBJECT = "subject";
+    final Logger logger = LoggerFactory.getLogger(SubjectAnnotator.class);
+
+    /**
+     * This constructor allows for the annotator to accept different properties to alter its behaviour.
+     *
+     * It doesn't seem to be documented anywhere, but a method in AnnotatorImplementations.java with signature
+     *      public Annotator custom(Properties properties, String property) { ... }
+     * allows for various constructor signatures to be implemented for a custom annotator.
+     * @param properties
+     */
+    public SubjectAnnotator(String name, Properties properties) {
+        String prefix = (name != null && !name.isEmpty())? name + ".":"";
+    }
+
+    @Override
+    public void annotate(Annotation annotation)  {
+    }
+
+    @Override
+    public Set<Requirement> requirementsSatisfied() {
+        Set<Requirement> requirementsSatisfied = new HashSet<>();
+        requirementsSatisfied.add(new Requirement(SUBJECT));
+        return requirementsSatisfied;
+    }
+
+    @Override
+    public Set<Requirement> requires() {
+        Set<Requirement> requirements = new HashSet<>();
+        // TODO: find out why it fails when requirements are set
+//        requirements.add(new Requirement(Annotator.STANFORD_PARSE));
+        return requirements;
+    }
+}
