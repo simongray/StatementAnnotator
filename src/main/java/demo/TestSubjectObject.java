4037b9978155643aabc507b0965f7ca9e5112527
Simon Gray <simongray@gmail.com>
2016-05-19 18:35:38 +0200

    updated demo

--- src/main/java/demo/TestSubjectObject.java
+++ src/main/java/demo/TestSubjectObject.java
@@ -11,24 +11,20 @@ public class TestSubjectObject {
         // setting up the pipeline
         Properties props = new Properties();
 //        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, gender, ner, parse, depparse, subjectobject, sentiment, sentimenttargets");  // long pipeline
-        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, depparse, subjectobject");  // short
+        props.setProperty("annotators", "tokenize, ssplit, pos, depparse, subjectobject");  // short
         props.setProperty("customAnnotatorClass.subjectobject", "nlp.SubjectObjectAnnotator");
         props.setProperty("customAnnotatorClass.sentimenttargets", "sentiment.SentimentTargetsAnnotator");
         props.put("ner.model", "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz");
 //        props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");  // fast, more memory usage
         props.setProperty("parse.model", "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");  // slow, less memory usage
-
-        DemoTimer.start("pipeline launch");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        DemoTimer.stop();
 
-        String example = "The amazing Henry doesn't like doing anything in particular";
-
-        // annotate a piece of text
+//        String example = "The amazing and furious Henry Larsson of Sweden doesn't like doing anything in particular.";
+//        String example = "The amazing Henry doesn't like doing anything in particular.";
+        String example = "Henry, Louis the Dragon and Sally Bates don't like doing anything in particular.";
+//        String example = "Henry, Louis the Dragon or Sally Bates don't like doing anything in particular.";
         Annotation annotation = new Annotation(example);
 
-        DemoTimer.start("annotating");
         pipeline.annotate(annotation);
-        DemoTimer.stop();
     }
 }
