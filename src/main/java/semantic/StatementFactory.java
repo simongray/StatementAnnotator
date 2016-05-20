681fa6b01cf7818e9e3aa7d69464f0135f0b394f
Simon Gray <simongray@gmail.com>
2016-05-20 11:31:29 +0200

    added class for creating statements from graphs

---
+++ src/main/java/semantic/StatementFactory.java
@@ -0,0 +1,14 @@
+package semantic;
+
+import edu.stanford.nlp.semgraph.SemanticGraph;
+
+import java.util.List;
+
+/**
+ * This class handles the construction of Statements.
+ */
+public class StatementFactory {
+    public static List<Statement> makeStatements(SemanticGraph graph) {
+        return null;
+    }
+}
