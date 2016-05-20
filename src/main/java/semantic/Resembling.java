a002734752df305c9210359ef84604c844f5a5d3
Simon Gray <simongray@gmail.com>
2016-05-20 11:27:30 +0200

    clarification

--- src/main/java/semantic/Resembling.java
+++ src/main/java/semantic/Resembling.java
@@ -3,7 +3,7 @@ package semantic;
 /**
  * This interface defines a way to compare objects using resemblance rather than strict equality or ordering.
  * It is useful for comparing natural language, which may express the same idea in different ways.
- * @param <T> the base class of the comparison.
+ * @param <T> the base class for the object of comparison.
  */
 public interface Resembling<T> {
     /**
