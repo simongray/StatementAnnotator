eb04c5b85639f668326c9d507d0326278dec0953
Simon Gray <simongray@gmail.com>
2016-05-23 16:23:28 +0200

    cleanup

--- src/main/java/statements/core/StatementFinder.java
+++ src/main/java/statements/core/StatementFinder.java
@@ -22,64 +22,32 @@ public  class StatementFinder {
      * @param sentence the sentence to look in
      * @return statements
      */
-     public static List<Statement> find(CoreMap sentence) {
-         logger.info("finding statements in sentence: " + sentence);
+    public static List<Statement> find(CoreMap sentence) {
+        logger.info("finding statements in sentence: " + sentence);
 
-         // saves resources
-         if (!isProper(sentence)) {
-             logger.info("sentence was not proper, no statements found");
-             return null;
-         }
+        // saves resources
+        if (!isProper(sentence)) {
+            logger.info("sentence was not proper, no statements found");
+            return null;
+        }
 
-         List<Statement> statements = new ArrayList<>();
-         SemanticGraph graph = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
+        SemanticGraph graph = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
 
-         Set<Subject> subjects = SubjectFinder.find(graph);
-         logger.info(subjects.toString());
-         for (Subject subject : subjects) {
-             logger.info("components: " + subject.getCompoundStrings());
-             logger.info("complete name: " + subject.getName());
-         }
+        // find statement components
+        Set<Subject> subjects = SubjectFinder.find(graph);
+        Set<Verb> verbs = VerbFinder.find(graph);
+        Set<DirectObject> directObjects = DirectObjectFinder.find(graph);
+        Set<IndirectObject> indirectObjects = IndirectObjectFinder.find(graph);
 
-//         for (Subject subject : subjects) {
-//             System.out.println();
-//             System.out.println(subject + " compares to other subjects in sentence in this way: ");
-//             for (Subject otherSubject : subjects) {
-//                 System.out.println(subject.resemble(otherSubject) + ": " + otherSubject);
-//             }
-//         }
+        // merge all components into same set before linking
+        Set<StatementComponent> components = new HashSet<>();
+        components.addAll(subjects);
+        components.addAll(verbs);
+        components.addAll(directObjects);
+        components.addAll(indirectObjects);
 
-         Set<Verb> verbs = VerbFinder.find(graph);
-         logger.info("verbs: " + verbs.toString());
-
-         Set<DirectObject> directObjects = DirectObjectFinder.find(graph);
-         Set<IndirectObject> indirectObjects = IndirectObjectFinder.find(graph);
-
-         logger.info("direct objects: " + directObjects);
-         logger.info("indirect objects: " + indirectObjects);
-
-         /*
-            Algorithm sketch:
-                   there is link-up method at the end that produces the actual subjects from the components,
-                   i.e. it gets the list of subjects, verbs, and objects and spits out statements
-                3. TODO: If no subjects can be found, the statement is assumed to be self-referencing
-                   and the verb phrase is analysed instead (e.g. "hate cycling!").
-
-                   TODO: capitalisation confusion in verb phrases
-                   it seems like parser results turn verb phrases like "hate cycling"
-                   into compound noun phrases if the first word is capitalised ("Hate") and can also be a noun.
-                   "Hated cycling" resolves correctly, though, since there is no way to confuse it.
-                   Will probably need to hack around that somehow to get the best results.
-         */
-
-         // merge all components into same set before linking
-         Set<StatementComponent> components = new HashSet<>();
-         components.addAll(subjects);
-         components.addAll(verbs);
-         components.addAll(directObjects);
-         components.addAll(indirectObjects);
-
-         return link(graph, components);
+        // link components to produce statements
+        return link(graph, components);
     }
 
     /**
@@ -97,7 +65,6 @@ public  class StatementFinder {
         for (StatementComponent component : components) {
             IndexedWord parentPrimary = graph.getParent(component.getPrimary());
             StatementComponent parent = getFromPrimary(parentPrimary, components);
-            logger.info("connection: " + component + " -> " + parent);
             childToParentMapping.put(component, parent);
         }
 
@@ -106,23 +73,22 @@ public  class StatementFinder {
             StatementComponent parent = childToParentMapping.getOrDefault(component, null);
 
             if (parent instanceof Subject && component instanceof Subject
-                || parent instanceof Verb && component instanceof Verb
-                || parent instanceof DirectObject && component instanceof DirectObject
-                || parent instanceof IndirectObject && component instanceof IndirectObject) {
+                    || parent instanceof Verb && component instanceof Verb
+                    || parent instanceof DirectObject && component instanceof DirectObject
+                    || parent instanceof IndirectObject && component instanceof IndirectObject) {
                 childToParentMapping.remove(component);
-                logger.info("removed connection: " + parent + " -> " + component);
             }
         }
 
         // create sets of components by following all connections
-        for (StatementComponent parent : childToParentMapping.keySet()) {
-            StatementComponent child = childToParentMapping.get(parent);
+        for (StatementComponent child : childToParentMapping.keySet()) {
+            StatementComponent parent = childToParentMapping.get(child);
             boolean found = false;
 
             // find existing component set to link up
             for (Set<StatementComponent> componentSet : componentSets) {
                 if (componentSet.contains(parent) || componentSet.contains(child)) {
-                    componentSet.add(parent);
+                    if (parent != null) componentSet.add(parent);
                     componentSet.add(child);
                     found = true;
                     break;
@@ -132,7 +98,7 @@ public  class StatementFinder {
             // create new component set if none found
             if (!found) {
                 Set<StatementComponent> newComponentSet = new HashSet<>();
-                newComponentSet.add(parent);
+                if (parent != null) newComponentSet.add(parent);
                 newComponentSet.add(child);
                 componentSets.add(newComponentSet);
             }
@@ -157,8 +123,7 @@ public  class StatementFinder {
                 }
             }
 
-            logger.info("new statement from: " + componentSet);
-
+            logger.info("linked statement from: " + componentSet);
             statements.add(new Statement(subject, verb, directObject, indirectObject));
         }
 
@@ -186,7 +151,7 @@ public  class StatementFinder {
      * @param sentence the sentence to test
      * @return proper or not
      */
-     private static boolean isProper(CoreMap sentence) {
+    private static boolean isProper(CoreMap sentence) {
         Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);  // TODO: do using depparse instead?
 
         for (Tree child : tree.children()) {
