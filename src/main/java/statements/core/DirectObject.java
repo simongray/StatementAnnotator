9fe4baecc2b7b8956dbe88bc0e0b806057083a64
Simon Gray <simongray@gmail.com>
2016-05-23 12:00:52 +0200

    fleshing out directObject based on code in subject class

--- src/main/java/statements/core/DirectObject.java
+++ src/main/java/statements/core/DirectObject.java
@@ -1,19 +1,33 @@
 package statements.core;
 
 import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.semgraph.SemanticGraph;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * The complete direct object of a natural language statement.
  */
 public class DirectObject implements Resembling<DirectObject> {
-    private IndexedWord directObject;
-    private boolean copula;
+    private final IndexedWord primary;
+    private Set<IndexedWord> secondary;
+    private final Set<IndexedWord> complete;
+    private final boolean copula;
+    private final Set<Set<IndexedWord>> compounds;
 
-    public DirectObject(IndexedWord directObject, boolean copula) {
-        this.directObject = directObject;
+    public DirectObject(IndexedWord primary, Set<IndexedWord> secondary, boolean copula, SemanticGraph graph) {
+        this.primary = primary;
+        this.secondary = secondary;
         this.copula = copula;
+        this.complete = graph.descendants(primary);
+
+        // recursively discover all compound objects
+        compounds = new HashSet<>();
+        compounds.add(StatementUtils.findCompoundComponents(primary, graph, null));
+        for (IndexedWord subject : secondary) {
+            compounds.add(StatementUtils.findCompoundComponents(subject, graph, null));
+        }
     }
 
     /**
@@ -21,7 +35,15 @@ public class DirectObject implements Resembling<DirectObject> {
      * @return the longest name possible
      */
     public String getName() {
-        return directObject.word();  // TODO: implement full version
+        return StatementUtils.join(complete);
+    }
+
+    /**
+     * The primary single direct object contained within the complete direct object.
+     * @return primary objct
+     */
+    public IndexedWord getPrimary() {
+        return primary;
     }
 
     /**
@@ -29,8 +51,19 @@ public class DirectObject implements Resembling<DirectObject> {
      * @return compounds
      */
     public Set<Set<IndexedWord>> getCompounds() {
-//        return new HashSet<>(compounds);
-        return null;  // TODO: implement
+        return compounds;
+    }
+
+    /**
+     * The strings of all of the compound direct objects.
+     * @return compound direct object strings
+     */
+    public Set<String> getCompoundStrings() {
+        Set<String> compoundSubjectNames = new HashSet<>();
+        for (Set<IndexedWord> compound : compounds) {
+            compoundSubjectNames.add(StatementUtils.join(compound));
+        }
+        return compoundSubjectNames;
     }
 
     /**
@@ -39,7 +72,21 @@ public class DirectObject implements Resembling<DirectObject> {
      * @return resemblance
      */
     @Override
-    public Resemblance resemble(DirectObject otherDirectObject) {
+    public Resemblance resemble(DirectObject otherObject) {
         return null;  // TODO
+    }
+
+    /**
+     * The amount of individual subjects contained within the complete direct object.
+     * @return objects count
+     */
+    public int size() {
+        return secondary.size() + 1;
+    }
+
+
+    @Override
+    public String toString() {
+        return getName() + " (" + size() + ")";
     }
 }
