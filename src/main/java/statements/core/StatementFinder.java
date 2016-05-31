121f421f124c949999c5d2c997054b5fd9918850
Simon Gray <simongray@gmail.com>
2016-05-31 17:32:13 +0200

    was actually expecting AbstractComponent, not potentially other statements

--- src/main/java/statements/core/StatementFinder.java
+++ src/main/java/statements/core/StatementFinder.java
@@ -33,7 +33,7 @@ public  class StatementFinder {
         Set<IndirectObject> indirectObjects = IndirectObjectFinder.find(graph);
 
         // merge all components into same set before linking
-        Set<StatementComponent> components = new HashSet<>();
+        Set<AbstractComponent> components = new HashSet<>();
         components.addAll(subjects);
         components.addAll(verbs);
         components.addAll(directObjects);
@@ -50,16 +50,16 @@ public  class StatementFinder {
      * @param components the various statement components found in sentence
      * @return statements
      */
-    private static List<Statement> link(SemanticGraph graph, Set<StatementComponent> components) {
-        Map<StatementComponent, StatementComponent> childToParentMapping = new HashMap<>();
-        Map<StatementComponent, StatementComponent> interStatementMapping = new HashMap<>();
-        Set<Set<StatementComponent>> componentSets = new HashSet<>();
+    private static List<Statement> link(SemanticGraph graph, Set<AbstractComponent> components) {
+        Map<AbstractComponent, AbstractComponent> childToParentMapping = new HashMap<>();
+        Map<AbstractComponent, AbstractComponent> interStatementMapping = new HashMap<>();
+        Set<Set<AbstractComponent>> componentSets = new HashSet<>();
         List<Statement> statements = new ArrayList<>();
 
         // find the direct parent for each component based on its primary word
-        for (StatementComponent component : components) {
+        for (AbstractComponent component : components) {
             IndexedWord parentPrimary = graph.getParent(component.getPrimary());
-            StatementComponent parent = getFromPrimary(parentPrimary, components);
+            AbstractComponent parent = getFromPrimary(parentPrimary, components);
 
             // keep track of linked statements
             if (parentPrimary != null && graph.reln(parentPrimary, component.getPrimary()).getShortName().equals(CCOMP_RELATION)) {
@@ -70,8 +70,8 @@ public  class StatementFinder {
         }
 
         // remove connections between identical component types
-        for (StatementComponent component : components) {
-            StatementComponent parent = childToParentMapping.getOrDefault(component, null);
+        for (AbstractComponent component : components) {
+            AbstractComponent parent = childToParentMapping.getOrDefault(component, null);
 
             if (parent instanceof Subject && component instanceof Subject
                     || parent instanceof Verb && component instanceof Verb
@@ -82,12 +82,12 @@ public  class StatementFinder {
         }
 
         // create sets of components by following all dependencies
-        for (StatementComponent child : childToParentMapping.keySet()) {
-            StatementComponent parent = childToParentMapping.get(child);
+        for (AbstractComponent child : childToParentMapping.keySet()) {
+            AbstractComponent parent = childToParentMapping.get(child);
             boolean found = false;
 
             // find existing component set to link up to
-            for (Set<StatementComponent> componentSet : componentSets) {
+            for (Set<AbstractComponent> componentSet : componentSets) {
                 if (componentSet.contains(parent) || componentSet.contains(child)) {
                     if (parent != null) componentSet.add(parent);
                     componentSet.add(child);
@@ -98,7 +98,7 @@ public  class StatementFinder {
 
             // create new component set if no parent was found
             if (!found) {
-                Set<StatementComponent> newComponentSet = new HashSet<>();
+                Set<AbstractComponent> newComponentSet = new HashSet<>();
                 if (parent != null) newComponentSet.add(parent);
                 newComponentSet.add(child);
                 componentSets.add(newComponentSet);
@@ -106,11 +106,11 @@ public  class StatementFinder {
         }
 
         // merge any component sets that intersect
-        Set<Set<StatementComponent>> mergedComponentSets = new HashSet<>();
-        for (Set<StatementComponent> componentSet : componentSets) {
-            Set<StatementComponent> mergedComponentSet = new HashSet<>(componentSet);
+        Set<Set<AbstractComponent>> mergedComponentSets = new HashSet<>();
+        for (Set<AbstractComponent> componentSet : componentSets) {
+            Set<AbstractComponent> mergedComponentSet = new HashSet<>(componentSet);
 
-            for (Set<StatementComponent> otherComponentSet : componentSets) {
+            for (Set<AbstractComponent> otherComponentSet : componentSets) {
                 if (intersect(mergedComponentSet, otherComponentSet)) {
                     mergedComponentSet.addAll(otherComponentSet);
                 }
@@ -120,13 +120,13 @@ public  class StatementFinder {
         }
 
         // create statements from component sets
-        for (Set<StatementComponent> componentSet : mergedComponentSets) {
+        for (Set<AbstractComponent> componentSet : mergedComponentSets) {
             Subject subject = null;
             Verb verb = null;
             DirectObject directObject = null;
             IndirectObject indirectObject = null;
 
-            for (StatementComponent component : componentSet) {
+            for (AbstractComponent component : componentSet) {
                 if (component instanceof Subject) {
                     subject = (Subject) component;
                 } else if (component instanceof Verb) {
@@ -156,8 +156,8 @@ public  class StatementFinder {
      * @param set2
      * @return
      */
-    private static boolean intersect(Set<StatementComponent> set1, Set<StatementComponent> set2) {
-        for (StatementComponent component : set1) {
+    private static boolean intersect(Set<AbstractComponent> set1, Set<AbstractComponent> set2) {
+        for (AbstractComponent component : set1) {
             if (set2.contains(component)) {
                 return true;
             }
@@ -167,14 +167,14 @@ public  class StatementFinder {
     }
 
     /**
-     * The StatementComponent corresponding to a primary word.
+     * The AbstractComponent corresponding to a primary word.
      *
      * @param primary primary word of component
      * @param components set of components to search in
      * @return component
      */
-    private static StatementComponent getFromPrimary(IndexedWord primary, Set<StatementComponent> components) {
-        for (StatementComponent component : components) {
+    private static AbstractComponent getFromPrimary(IndexedWord primary, Set<AbstractComponent> components) {
+        for (AbstractComponent component : components) {
             if (component.getPrimary().equals(primary)) return component;
         }
 
