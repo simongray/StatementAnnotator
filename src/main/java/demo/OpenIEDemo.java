2ee3d767bad1b154b51c02f2e12f3d25712d5227
Simon Gray <simongray@gmail.com>
2016-06-02 16:36:49 +0200

    interestingly, openie has really poor results with the sentences I've been programming against

--- src/main/java/demo/OpenIEDemo.java
+++ src/main/java/demo/OpenIEDemo.java
@@ -40,12 +40,13 @@ public class OpenIEDemo {
             text = IOUtils.slurpFile(args[0]);
         } else {
 //            text = "Obama was born in Hawaii. He is our president.";
-            text =    "Recently moved here with my girlfriend and we have found that it is quite manageable. Here's our solution: " +
-                    "Use an air quality app. We use the one for http://aqicn.org/city/beijing/. I just have a widget on my Android phone that says the current AQI from the nearest measuring station. Our house rule is to use masks when it's 200+, although my girlfriend often does it from 150+. Anyway, just make your own rule and stick to it. " +
-                    "Bought some 3M 95N-rated face masks for smoggy days. Just keep one in my bag at all times as the wind can direction and smoggify the nicest days in a couple of hours sometimes. You can get some nice re-usable masks where you can change the filter too. " +
-                    "We also got two Xiaomi air purifiers that work quite well. They are smartphone-connected and always on, except when we're out during weekdays. The app allows you to check the latest PM2.5 index inside your flat and automate the purifiers. " +
-                    "Before you go, familiarise yourself with the air quality in your own area so you have a means of comparison. If you live in a city (e.g. Copenhagen) you might be surprised that some days it can actually be quite polluted in Western cities too. I sure was (I come from Copenhagen, Denmark). Of course, Western cities usually don't have those crazy smog days. " +
-                    "Don't worry too much. Sometimes there's no smog for a whole week, sometime's it lasts for a whole week and you'll just stay mostly indoors and use masks when outside. You'll get used to it.";
+            text = "He doesn't like doing anything. I think she's mad. I don't care whether she likes me. She says that they should go. I don't care whether or not they come.";
+//            text =    "Recently moved here with my girlfriend and we have found that it is quite manageable. Here's our solution: " +
+//                    "Use an air quality app. We use the one for http://aqicn.org/city/beijing/. I just have a widget on my Android phone that says the current AQI from the nearest measuring station. Our house rule is to use masks when it's 200+, although my girlfriend often does it from 150+. Anyway, just make your own rule and stick to it. " +
+//                    "Bought some 3M 95N-rated face masks for smoggy days. Just keep one in my bag at all times as the wind can direction and smoggify the nicest days in a couple of hours sometimes. You can get some nice re-usable masks where you can change the filter too. " +
+//                    "We also got two Xiaomi air purifiers that work quite well. They are smartphone-connected and always on, except when we're out during weekdays. The app allows you to check the latest PM2.5 index inside your flat and automate the purifiers. " +
+//                    "Before you go, familiarise yourself with the air quality in your own area so you have a means of comparison. If you live in a city (e.g. Copenhagen) you might be surprised that some days it can actually be quite polluted in Western cities too. I sure was (I come from Copenhagen, Denmark). Of course, Western cities usually don't have those crazy smog days. " +
+//                    "Don't worry too much. Sometimes there's no smog for a whole week, sometime's it lasts for a whole week and you'll just stay mostly indoors and use masks when outside. You'll get used to it.";
 
         }
         Annotation doc = new Annotation(text);
