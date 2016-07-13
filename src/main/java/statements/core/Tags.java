bc4033ad8d9949c5684f6a2db138e01c65be5420
Simon Gray <simongray@gmail.com>
2016-07-13 14:25:40 +0200

    also using POS tags for statements now

---
+++ src/main/java/statements/core/Tags.java
@@ -0,0 +1,44 @@
+package statements.core;
+
+import edu.stanford.nlp.ling.IndexedWord;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Tags {
+    public static final String VB = "vb";
+    public static final String VBP = "vbp";
+
+    /**
+     * Outgoing relations which are stored internally in any AbstractComponent, but not shown/used by default.
+     *
+     * An example is the NEG relation, which is separated out from the other words,
+     * but used to adjust results when doing comparisons.
+     */
+    public static final Set<String> VERBS = new HashSet<>();
+    static {
+        VERBS.add(VB);
+        VERBS.add(VBP);
+    }
+
+    /**
+     * Reduce a set of words based on their part-of-speech tags.
+     *
+     * @param words
+     * @param allowedtags
+     * @return
+     */
+    public static Set<IndexedWord> reduce(Set<IndexedWord> words, Set<String> allowedtags) {
+        Set<IndexedWord> badWords = new HashSet<>();
+        Set<IndexedWord> reducedWords = new HashSet<>(words);
+
+        for (IndexedWord word : words) {
+            if (!allowedtags.contains(word.tag())) badWords.add(word);
+        }
+
+        reducedWords.removeAll(badWords);
+
+        return reducedWords;
+    }
+}
