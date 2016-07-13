e628aba89e2c8b4931cacdebb6d2a00a0b633ac8
Simon Gray <simongray@gmail.com>
2016-07-13 10:50:13 +0200

    added profile demo script

---
+++ src/main/java/demo/TestProfile.java
@@ -0,0 +1,92 @@
+package demo;
+
+
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import org.json.JSONArray;
+import reddit.MarkdownStripper;
+import reddit.RedditCommentProcessor;
+import sentiment.ComplexSentiment;
+import sentiment.SentimentProfile;
+import sentiment.SentimentTarget;
+import statements.annotations.StatementsAnnotation;
+import statements.core.Statement;
+import statements.core.StatementUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.*;
+
+
+public class TestProfile {
+    private final static String ENGLISH = "en";
+    private final static String DANISH = "da";
+
+    public static void main(String[] args) throws IOException {
+        // setting up the pipeline
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, depparse, statement");  // short pipeline
+        props.setProperty("customAnnotatorClass.statement", "statements.StatementAnnotator");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        MarkdownStripper stripper = new MarkdownStripper();
+        String content = RedditCommentProcessor.readFile("src/main/java/demo/data/data.json", Charset.defaultCharset());
+//        String content = RedditCommentProcessor.readFile("src/main/java/demo/data/mark_comment_history.json", Charset.defaultCharset());
+        JSONArray jsonArray = new JSONArray(content);
+
+        List<String> englishComments = new ArrayList<>();
+        List<String> danishComments = new ArrayList<>();
+        List<String> otherComments = new ArrayList<>();
+
+        List<Statement> statements = new ArrayList<>();
+
+        for (Object obj : jsonArray) {
+            String comment = (String) obj;
+
+            // strip markdown
+            comment = stripper.strip(comment);
+
+            // identify language
+            DemoTimer.start("language identification");
+            String language = RedditCommentProcessor.getLanguage(comment);
+            DemoTimer.stop();
+
+            int endIndex = comment.length() < 30? comment.length() - 1: 30;
+            System.out.println(language + " -> " + comment.substring(0, endIndex));
+
+            if (language.equals(ENGLISH)) {
+                // annotate a piece of text
+                DemoTimer.start("creating annotation");
+                Annotation annotation = new Annotation(comment);
+                pipeline.annotate(annotation);
+                DemoTimer.stop();
+
+                List<CoreMap> sentences = annotation.get(SentencesAnnotation.class);
+
+                for (CoreMap sentence : sentences) {
+                    Set<Statement> sentenceStatements = sentence.get(StatementsAnnotation.class);
+                    System.out.println(sentence);
+                    StatementUtils.printStatements(sentenceStatements);
+                    if (sentenceStatements != null) statements.addAll(sentenceStatements);
+                }
+
+                englishComments.add(comment);
+            } else if (language.equals(DANISH)) {
+                danishComments.add(comment);
+            } else {
+                otherComments.add(comment);
+            }
+        }
+
+        System.out.println("english comments: " + englishComments.size());
+        System.out.println("danish comments: " + danishComments.size());
+        System.out.println("unknown comments: " + otherComments.size());
+        System.out.println("statements: " + statements.size());
+
+        DemoTimer.start("building profile...");
+//        SentimentProfile testProfile = new SentimentProfile(annotations);
+        DemoTimer.stop();
+    }
+}
