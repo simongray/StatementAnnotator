c0cbb9288255b052c2752c4ec415ac1b1822d090
Simon Gray <simongray@gmail.com>
2016-07-19 15:33:53 +0200

    cleand up special words

--- src/main/java/statements/core/SpecialWords.java
+++ src/main/java/statements/core/SpecialWords.java
@@ -5,26 +5,38 @@ import java.util.Set;
 
 public class SpecialWords {
     // ref: https://learnenglish.britishcouncil.org/en/english-grammar/determiners-and-quantifiers
+    public static final Set<String> POSSESSIVES = new HashSet<>();
+    static {
+        // possessives
+        POSSESSIVES.add("my");
+        POSSESSIVES.add("your");
+        POSSESSIVES.add("his");
+        POSSESSIVES.add("her");
+        POSSESSIVES.add("its");
+        POSSESSIVES.add("our");
+        POSSESSIVES.add("their");
+        POSSESSIVES.add("whose");
+    }
+
+    public static final Set<String> DEMONSTRATIVES = new HashSet<>();
+    static {
+        // demonstratives
+        DEMONSTRATIVES.add("this");
+        DEMONSTRATIVES.add("that");
+        DEMONSTRATIVES.add("these");
+        DEMONSTRATIVES.add("those");
+    }
+
     public static final Set<String> SPECIFIC_DETERMINERS = new HashSet<>();
     static {
         // definite article
         SPECIFIC_DETERMINERS.add("the");
 
         // possessives
-        SPECIFIC_DETERMINERS.add("my");
-        SPECIFIC_DETERMINERS.add("your");
-        SPECIFIC_DETERMINERS.add("his");
-        SPECIFIC_DETERMINERS.add("her");
-        SPECIFIC_DETERMINERS.add("its");
-        SPECIFIC_DETERMINERS.add("our");
-        SPECIFIC_DETERMINERS.add("their");
-        SPECIFIC_DETERMINERS.add("whose");
+        SPECIFIC_DETERMINERS.addAll(POSSESSIVES);
 
         // demonstratives
-        SPECIFIC_DETERMINERS.add("this");
-        SPECIFIC_DETERMINERS.add("that");
-        SPECIFIC_DETERMINERS.add("these");
-        SPECIFIC_DETERMINERS.add("those");
+        SPECIFIC_DETERMINERS.addAll(DEMONSTRATIVES);
 
         // interrogatives
         SPECIFIC_DETERMINERS.add("which");
